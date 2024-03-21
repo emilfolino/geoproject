e178b7edf3bac088dc5b5469de093d2d9bcebf7d
Emil Folino <emilfolino@gmail.com>
2024-03-21 13:11:12 +0100

    Added base in app.mjs

---
+++ app.mjs
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const port = process.env.PORT || 8866;
+const app = express();
+
+app.use(cors());
+app.options('*', cors());
+
+app.disable('x-powered-by');
+
+// don't show the log when it is test
+if (process.env.NODE_ENV !== 'test') {
+    // use morgan to log at command line
+    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send("docs"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
